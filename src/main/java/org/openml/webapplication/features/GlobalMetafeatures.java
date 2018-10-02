a9b99a73a4ef1496afcf36574f94ff39e453b14f
JoaquinVanschoren <Joaquin.Vanschoren@gmail.com>
2018-10-03 01:41:30 +0200

    whitespace fixes

--- src/main/java/org/openml/webapplication/features/GlobalMetafeatures.java
+++ src/main/java/org/openml/webapplication/features/GlobalMetafeatures.java
@@ -12,91 +12,92 @@ import java.util.Arrays;
 import java.util.List;
 
 public class GlobalMetafeatures {
-    private final String preprocessingPrefix = "-E \"weka.attributeSelection.CfsSubsetEval -P 1 -E 1\" -S \"weka.attributeSelection.BestFirst -D 1 -N 5\" -W ";
-    private final String cp1NN = "weka.classifiers.lazy.IBk";
-    private final String cpNB = "weka.classifiers.bayes.NaiveBayes";
-    private final String cpASC = "weka.classifiers.meta.AttributeSelectedClassifier";
-    private final String cpDS = "weka.classifiers.trees.DecisionStump";
-    //private final StreamCharacterizer[] streamCharacterizers;
+	private final String preprocessingPrefix = "-E \"weka.attributeSelection.CfsSubsetEval -P 1 -E 1\" -S \"weka.attributeSelection.BestFirst -D 1 -N 5\" -W ";
+	private final String cp1NN = "weka.classifiers.lazy.IBk";
+	private final String cpNB = "weka.classifiers.bayes.NaiveBayes";
+	private final String cpASC = "weka.classifiers.meta.AttributeSelectedClassifier";
+	private final String cpDS = "weka.classifiers.trees.DecisionStump";
+	//private final StreamCharacterizer[] streamCharacterizers;
 
-    private ArrayList<Characterizer> batchCharacterizers;
-    private int expectedQualities;
-    private ArrayList<String> expectedIds = new ArrayList<>();
+	private ArrayList<Characterizer> batchCharacterizers;
+	private int expectedQualities;
+	private ArrayList<String> expectedIds = new ArrayList<>();
 
-    /**
-     * Collection of characterizers
-     * 
-     * @param characterizer Class name of requested characterizer. Can also be 'fast' 
-     * for everything except landmarkers. If null, all characterizers are added.
-     * @param window_size for streaming landmarkers
-     * @throws Exception
-     */
-    public GlobalMetafeatures(String characterizer, Integer window_size) throws Exception {
-    	
-    	ArrayList<Characterizer> batchCharacterizers = new ArrayList<Characterizer>();
-    	batchCharacterizers.add(new SimpleMetaFeatures()); // always included because others depend on them
+	/**
+	 * Collection of characterizers
+	 * 
+	 * @param characterizer Class name of requested characterizer. Can also be 'fast' 
+	 * for everything except landmarkers. If null, all characterizers are added.
+	 * @param window_size for streaming landmarkers
+	 * @throws Exception
+	 */
+	public GlobalMetafeatures(String characterizer, Integer window_size) throws Exception {
+
+		ArrayList<Characterizer> batchCharacterizers = new ArrayList<Characterizer>();
+		batchCharacterizers.add(new SimpleMetaFeatures()); // always included because others depend on them
 
 		// Add characterizer of choice
-    	if (characterizer != null && !characterizer.equals("GenericLandmarker") 
-    			&& !characterizer.equals("fast") && !characterizer.equals("SimpleMetaFeatures")) {
-    		Characterizer ch = (Characterizer) Class.forName(characterizer).newInstance();
-    		batchCharacterizers.add(ch);
-    	} 
-    	
+		if (characterizer != null && !characterizer.equals("GenericLandmarker") 
+				&& !characterizer.equals("fast") && !characterizer.equals("SimpleMetaFeatures")) {
+			Characterizer ch = (Characterizer) Class.forName(characterizer).newInstance();
+			batchCharacterizers.add(ch);
+		} 
+
 		// Add all 'fast' characterizers
-        if (characterizer == null || characterizer.equals("fast")) {
-        	batchCharacterizers.add(new Statistical());
-        	batchCharacterizers.add(new NominalAttDistinctValues());
-        	batchCharacterizers.add(new AttributeEntropy());
-        }
-        
+		if (characterizer == null || characterizer.equals("fast")) {
+			batchCharacterizers.add(new Statistical());
+			batchCharacterizers.add(new NominalAttDistinctValues());
+			batchCharacterizers.add(new AttributeEntropy());
+		}
+
 		// Add landmarkers
-        if (characterizer == null || characterizer.equals("GenericLandmarker")) {
-        	batchCharacterizers.add(new GenericLandmarker("kNN1N", cp1NN, 2, null));
-        	batchCharacterizers.add(new GenericLandmarker("NaiveBayes", cpNB, 2, null));
-        	batchCharacterizers.add(new GenericLandmarker("DecisionStump", cpDS, 2, null));
-        	batchCharacterizers.add(new GenericLandmarker("CfsSubsetEval_kNN1N", cpASC, 2, Utils.splitOptions(preprocessingPrefix + cp1NN)));
-        	batchCharacterizers.add(new GenericLandmarker("CfsSubsetEval_NaiveBayes", cpASC, 2, Utils.splitOptions(preprocessingPrefix + cpNB)));
-        	batchCharacterizers.add(new GenericLandmarker("CfsSubsetEval_DecisionStump", cpASC, 2, Utils.splitOptions(preprocessingPrefix + cpDS)));
-        
-		    String zeros = "0";
-		    for( int i = 1; i <= 3; ++i ) {
-		        zeros += "0";
-		        String[] j48Option = { "-C", "." + zeros + "1" };
-		        batchCharacterizers.add(new GenericLandmarker("J48." + zeros + "1.", "weka.classifiers.trees.J48", 2, j48Option));
-		
-		        String[] repOption = { "-L", "" + i };
-		        batchCharacterizers.add(new GenericLandmarker("REPTreeDepth" + i, "weka.classifiers.trees.REPTree", 2, repOption));
-		
-		        String[] randomtreeOption = { "-depth", "" + i };
-		        batchCharacterizers.add(new GenericLandmarker("RandomTreeDepth" + i, "weka.classifiers.trees.RandomTree", 2, randomtreeOption));
-		    }
-        }
-        
-        for(Characterizer c : batchCharacterizers) {
-            expectedQualities += c.getNumMetaFeatures();
-            expectedIds.addAll(Arrays.asList(c.getIDs()));
-        }
+		if (characterizer == null || characterizer.equals("GenericLandmarker")) {
+			batchCharacterizers.add(new GenericLandmarker("kNN1N", cp1NN, 2, null));
+			batchCharacterizers.add(new GenericLandmarker("NaiveBayes", cpNB, 2, null));
+			batchCharacterizers.add(new GenericLandmarker("DecisionStump", cpDS, 2, null));
+			batchCharacterizers.add(new GenericLandmarker("CfsSubsetEval_kNN1N", cpASC, 2, Utils.splitOptions(preprocessingPrefix + cp1NN)));
+			batchCharacterizers.add(new GenericLandmarker("CfsSubsetEval_NaiveBayes", cpASC, 2, Utils.splitOptions(preprocessingPrefix + cpNB)));
+			batchCharacterizers.add(new GenericLandmarker("CfsSubsetEval_DecisionStump", cpASC, 2, Utils.splitOptions(preprocessingPrefix + cpDS)));
 
-   /*     streamCharacterizers = new StreamCharacterizer[]{new ChangeDetectors(window_size)};
+			String zeros = "0";
+			for( int i = 1; i <= 3; ++i ) {
+				zeros += "0";
+				String[] j48Option = { "-C", "." + zeros + "1" };
+				batchCharacterizers.add(new GenericLandmarker("J48." + zeros + "1.", "weka.classifiers.trees.J48", 2, j48Option));
 
-        for (StreamCharacterizer streamCharacterizer: streamCharacterizers) {
-            expectedQualities += streamCharacterizer.getNumMetaFeatures();
-            expectedIds.addAll(Arrays.asList(streamCharacterizer.getIDs()));
-        } */
-    }
+				String[] repOption = { "-L", "" + i };
+				batchCharacterizers.add(new GenericLandmarker("REPTreeDepth" + i, "weka.classifiers.trees.REPTree", 2, repOption));
 
-    public int getExpectedQualities() {
-        return expectedQualities;
-    }
+				String[] randomtreeOption = { "-depth", "" + i };
+				batchCharacterizers.add(new GenericLandmarker("RandomTreeDepth" + i, "weka.classifiers.trees.RandomTree", 2, randomtreeOption));
+			}
+		}
 
-    public List<String> getExpectedIds() {
-        return expectedIds;
-    }
+		for(Characterizer c : batchCharacterizers) {
+			expectedQualities += c.getNumMetaFeatures();
+			expectedIds.addAll(Arrays.asList(c.getIDs()));
+		}
 
-    public List<Characterizer> getCharacterizers(){
-        return batchCharacterizers;
-    }
+		/**     
+		streamCharacterizers = new StreamCharacterizer[]{new ChangeDetectors(window_size)};
+		for (StreamCharacterizer streamCharacterizer: streamCharacterizers) {
+			expectedQualities += streamCharacterizer.getNumMetaFeatures();
+			expectedIds.addAll(Arrays.asList(streamCharacterizer.getIDs()));
+		}
+		*/
+	}
 
-    //public StreamCharacterizer[] getStreamCharacterizers() { return streamCharacterizers; }
+	public int getExpectedQualities() {
+		return expectedQualities;
+	}
+
+	public List<String> getExpectedIds() {
+		return expectedIds;
+	}
+
+	public List<Characterizer> getCharacterizers(){
+		return batchCharacterizers;
+	}
+
+	//public StreamCharacterizer[] getStreamCharacterizers() { return streamCharacterizers; }
 }
