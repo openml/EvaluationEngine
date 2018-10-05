e2bb242d0a22384caf522442919f4be05251f276
janvanrijn <janvanrijn@gmail.com>
2018-10-05 10:12:56 -0400

    fixes indentation and small bug

--- src/test/java/org/openml/webapplication/fantail/TestFantailConnector.java
+++ src/test/java/org/openml/webapplication/fantail/TestFantailConnector.java
@@ -18,62 +18,61 @@ import org.openml.webapplication.testutils.DatasetFactory;
 import weka.core.Instances;
 
 public class TestFantailConnector {
-	
+
 	private static Method makeMethodAccessible() throws NoSuchMethodException, SecurityException {
 		Class[] cArgClasses = new Class[6];
-        cArgClasses[0] = int.class;
-        cArgClasses[1] = Instances.class;
-        cArgClasses[2] = List.class;
-        cArgClasses[3] = List.class;
-        cArgClasses[4] = String.class;
-        cArgClasses[5] = List.class;
-        
-        Method method = FantailConnector.class.getDeclaredMethod("extractFeatures", cArgClasses);
+		cArgClasses[0] = int.class;
+		cArgClasses[1] = Instances.class;
+		cArgClasses[2] = List.class;
+		cArgClasses[3] = List.class;
+		cArgClasses[4] = String.class;
+		cArgClasses[5] = List.class;
+
+		Method method = FantailConnector.class.getDeclaredMethod("extractFeatures", cArgClasses);
 		method.setAccessible(true);
 		return method;
 	}
-	
+
 	@Test
 	public final void testXorNumeric() throws Exception {
 		List<Characterizer> characterizers = CharacterizerFactory.all(null);
 		FantailConnector connector = new FantailConnector(null, characterizers);
-        
-        String[] target = {"y"};
-        Object[] cArgValues = new Object[6];
-        cArgValues[0] = -1;
-        cArgValues[1] = DatasetFactory.getXORNumericNoClass();
-        cArgValues[2] = new ArrayList<String>(Arrays.asList(target));
-        cArgValues[3] = null;
-        cArgValues[4] = null;
-        cArgValues[5] = new ArrayList<String>(Arrays.asList(new String[0]));
-		
+
+		String[] target = { "y" };
+		Object[] cArgValues = new Object[6];
+		cArgValues[0] = -1;
+		cArgValues[1] = DatasetFactory.getXORNumericNoClass();
+		cArgValues[2] = new ArrayList<String>(Arrays.asList(target));
+		cArgValues[3] = null;
+		cArgValues[4] = null;
+		cArgValues[5] = new ArrayList<String>(Arrays.asList(new String[0]));
+
 		Method method = makeMethodAccessible();
-		
+
 		Set<Quality> result = (Set<Quality>) method.invoke(connector, cArgValues);
-		
+
 		assertEquals(CharacterizerFactory.getExpectedQualities(characterizers).size(), result.size());
 	}
-	
+
 	@Test
 	public final void testXorNominal() throws Exception {
 		List<Characterizer> characterizers = CharacterizerFactory.all(null);
 		FantailConnector connector = new FantailConnector(null, characterizers);
-        
-        String[] target = {"class"};
-        Object[] cArgValues = new Object[6];
-        cArgValues[0] = -1;
-        cArgValues[1] = DatasetFactory.getXORNominalNoClass();
-        cArgValues[2] = new ArrayList<String>(Arrays.asList(target));
-        cArgValues[3] = null;
-        cArgValues[4] = null;
-        cArgValues[5] = new ArrayList<String>(Arrays.asList(new String[0]));
-		
+
+		String[] target = { "class" };
+		Object[] cArgValues = new Object[6];
+		cArgValues[0] = -1;
+		cArgValues[1] = DatasetFactory.getXORNominalNoClass();
+		cArgValues[2] = new ArrayList<String>(Arrays.asList(target));
+		cArgValues[3] = null;
+		cArgValues[4] = null;
+		cArgValues[5] = new ArrayList<String>(Arrays.asList(new String[0]));
+
 		Method method = makeMethodAccessible();
-		
+
 		Set<Quality> result = (Set<Quality>) method.invoke(connector, cArgValues);
-		
+
 		assertEquals(CharacterizerFactory.getExpectedQualities(characterizers).size(), result.size());
 	}
-	
-	
+
 }
